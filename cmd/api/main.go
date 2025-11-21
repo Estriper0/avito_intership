064dcfb75d10573c5d9ffb339964815a3b532bef
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-21 18:07:47 +0300

    Add reading of the .env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,17 @@ import (
 	"github.com/Estriper0/avito_intership/internal/app"
 	"github.com/Estriper0/avito_intership/internal/config"
 	"github.com/Estriper0/avito_intership/internal/logger"
+	"github.com/joho/godotenv"
 )
 
 const configPath = "configs/config.yaml"
 
 func main() {
+	_ = godotenv.Load()
+
 	config := config.New(configPath)
 	logger := logger.GetLogger(config.App.Env)
+	
 	app := app.New(logger, config)
 	app.Run()
 }
