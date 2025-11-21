ff659b394423119df9d5259d821a141589ffb3dc
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-21 18:09:26 +0300

    Add drop index

--- migrations/000001_init_tables.down.sql
+++ migrations/000001_init_tables.down.sql
@@ -2,3 +2,8 @@ DROP TABLE IF EXISTS teams;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS pull_requests;
 DROP TABLE IF EXISTS pull_requests_statuses;
+
+DROP INDEX IF EXISTS idx_users_team_id;
+DROP INDEX IF EXISTS idx_pr_author_id;
+DROP INDEX IF EXISTS idx_pr_reviewers_pr_id;
+DROP INDEX IF EXISTS idx_pr_reviewers_user_id;
