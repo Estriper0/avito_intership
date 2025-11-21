299bed06f84dbe57cd517a0d7036727e7c5de0c2
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-21 18:03:15 +0300

    Add service interfaces

---
+++ internal/service/service.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Estriper0/avito_intership/internal/handlers/dto"
+)
+
+type IUserService interface {
+	SetIsActive(ctx context.Context, req *dto.SetIsActiveRequest) (*dto.UserResponse, error)
+	GetReview(ctx context.Context, userId string) ([]dto.ReviewResponse, error)
+}
+
+type ITeamService interface {
+	Add(ctx context.Context, team *dto.Team) (int, error)
+	Get(ctx context.Context, teamName string) (*dto.Team, error)
+}
+
+type IPullRequestService interface {
+	Create(ctx context.Context, pr *dto.PrCreateRequest) (*dto.PullRequest, error)
+	Merge(ctx context.Context, prId string) (*dto.MergeResponse, error)
+	Reassign(ctx context.Context, req *dto.ReassignRequest) (*dto.ReassignResponse, error)
+}
