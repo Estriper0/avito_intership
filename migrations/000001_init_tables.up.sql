e74522e0e6d27044b74692ec6188ffdbe9c49f9c
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-21 17:59:04 +0300

    Create migrations

---
+++ migrations/000001_init_tables.up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR(30) PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    team_id INTEGER NOT NULL REFERENCES teams(id),
+    is_active BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pull_requests_statuses (
+    id INTEGER PRIMARY KEY,
+    status VARCHAR(30) UNIQUE NOT NULL
+);
+
+INSERT INTO pull_requests_statuses (id, status) VALUES
+    (1, 'OPEN'),
+    (2, 'MERGED');
+
+CREATE TABLE IF NOT EXISTS pull_requests (
+    pr_id VARCHAR(30) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    author_id VARCHAR(30) NOT NULL REFERENCES users(user_id),
+    status_id INTEGER NOT NULL DEFAULT 1 REFERENCES pull_requests_statuses(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    merged_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS pull_requests_reviewers (
+    pr_id VARCHAR(30) REFERENCES pull_requests(pr_id),
+    user_id VARCHAR(30) REFERENCES users(user_id)
+);
+
+CREATE INDEX idx_users_team_id ON users(team_id);
+CREATE INDEX idx_pr_author_id ON pull_requests(author_id);
+CREATE INDEX idx_pr_reviewers_pr_id ON pull_requests_reviewers(pr_id);
+CREATE INDEX idx_pr_reviewers_user_id ON pull_requests_reviewers(user_id);
