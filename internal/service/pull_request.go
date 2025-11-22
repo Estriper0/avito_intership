6f4e6dba0083a3313ca20954d0473fec093182d4
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-22 11:23:09 +0300

    Added transactions to pull request creation

--- internal/service/pull_request.go
+++ internal/service/pull_request.go
@@ -9,92 +9,102 @@ import (
 	"github.com/Estriper0/avito_intership/internal/handlers/dto"
 	"github.com/Estriper0/avito_intership/internal/models"
 	"github.com/Estriper0/avito_intership/internal/repository"
+	"github.com/avito-tech/go-transaction-manager/trm/v2/manager"
 )
 
 type PullRequestService struct {
-	prRepo   repository.IPullRequestRepo
-	userRepo repository.IUserRepo
-	logger   *slog.Logger
+	prRepo    repository.IPullRequestRepo
+	userRepo  repository.IUserRepo
+	trManager *manager.Manager
+	logger    *slog.Logger
 }
 
-func NewPullRequestService(prRepo repository.IPullRequestRepo, userRepo repository.IUserRepo, logger *slog.Logger) *PullRequestService {
+func NewPullRequestService(prRepo repository.IPullRequestRepo, userRepo repository.IUserRepo, trManager *manager.Manager, logger *slog.Logger) *PullRequestService {
 	return &PullRequestService{
-		prRepo:   prRepo,
-		userRepo: userRepo,
-		logger:   logger,
+		prRepo:    prRepo,
+		userRepo:  userRepo,
+		trManager: trManager,
+		logger:    logger,
 	}
 }
 
 func (s *PullRequestService) Create(ctx context.Context, pr *dto.PrCreateRequest) (*dto.PullRequest, error) {
-	exists, err := s.userRepo.ExistsById(ctx, pr.AuthorId)
-	if err != nil {
-		s.logger.Error("PullRequestService.Create:userRepo.ExistsById - Internal error", slog.String("error", err.Error()))
-		return nil, ErrInternal
-	} else if !exists {
-		return nil, ErrNotFound
-	}
+	var resp *dto.PullRequest
+	//Do everything in a transaction
+	err := s.trManager.Do(ctx, func(ctx context.Context) error {
+		exists, err := s.userRepo.ExistsById(ctx, pr.AuthorId)
+		if err != nil {
+			s.logger.Error("PullRequestService.Create:userRepo.ExistsById - Internal error", slog.String("error", err.Error()))
+			return ErrInternal
+		} else if !exists {
+			return ErrNotFound
+		}
 
-	//Getting all active users from a user's team without a user
-	activeUsers, err := s.userRepo.GetActiveTeamMembersById(ctx, pr.AuthorId)
-	if err != nil {
-		s.logger.Error("PullRequestService.Create:userRepo.GetActiveTeamMembersById - Internal error", slog.String("error", err.Error()))
-		return nil, ErrInternal
-	}
+		//Getting all active users from a user's team without a user
+		activeUsers, err := s.userRepo.GetActiveTeamMembersById(ctx, pr.AuthorId)
+		if err != nil {
+			s.logger.Error("PullRequestService.Create:userRepo.GetActiveTeamMembersById - Internal error", slog.String("error", err.Error()))
+			return ErrInternal
+		}
 
-	p, err := s.prRepo.Create(ctx, &models.PullRequest{
-		PrId:     pr.PrId,
-		Name:     pr.PrName,
-		AuthorId: pr.AuthorId,
+		p, err := s.prRepo.Create(ctx, &models.PullRequest{
+			PrId:     pr.PrId,
+			Name:     pr.PrName,
+			AuthorId: pr.AuthorId,
+		})
+		if err != nil {
+			if errors.Is(err, repository.ErrAlreadyExists) {
+				return ErrPullRequestALreadyExists
+			}
+			s.logger.Error("PullRequestService.Create:prRepo.Create - Internal error", slog.String("error", err.Error()))
+			return ErrInternal
+		}
+
+		//Getting a status name
+		status, err := s.prRepo.GetStatusById(ctx, p.StatusId)
+		if err != nil {
+			if errors.Is(err, repository.ErrNotFound) {
+				return ErrNotFound
+			}
+			s.logger.Error("PullRequestService.Create:prRepo.GetStatusById - Internal error", slog.String("error", err.Error()))
+			return ErrInternal
+		}
+
+		//How many users will we assign as reviewers
+		var reviewersId []string
+		var count int
+		if len(activeUsers) > 2 {
+			count = 2
+		} else {
+			count = len(activeUsers)
+		}
+
+		//Randomly shuffle the slice of active users and assign reviewers
+		perm := rand.Perm(len(activeUsers))
+		for i := 0; i < count; i++ {
+			reviewersId = append(reviewersId, activeUsers[perm[i]].UserId)
+		}
+
+		err = s.prRepo.AddReviewers(ctx, pr.PrId, reviewersId)
+		if err != nil {
+			if errors.Is(err, repository.ErrAlreadyExists) {
+				return ErrPullRequestALreadyExists
+			}
+			s.logger.Error("PullRequestService.Create:prRepo.AddReviewers - Internal error", slog.String("error", err.Error()))
+			return ErrInternal
+		}
+
+		resp = &dto.PullRequest{
+			PrId:              p.PrId,
+			PrName:            p.Name,
+			AuthorId:          p.AuthorId,
+			Status:            status,
+			AssignedReviewers: reviewersId,
+		}
+
+		return nil
 	})
-	if err != nil {
-		if errors.Is(err, repository.ErrAlreadyExists) {
-			return nil, ErrPullRequestALreadyExists
-		}
-		s.logger.Error("PullRequestService.Create:prRepo.Create - Internal error", slog.String("error", err.Error()))
-		return nil, ErrInternal
-	}
-
-	//Getting a status name
-	status, err := s.prRepo.GetStatusById(ctx, p.StatusId)
-	if err != nil {
-		if errors.Is(err, repository.ErrNotFound) {
-			return nil, ErrNotFound
-		}
-		s.logger.Error("PullRequestService.Create:prRepo.GetStatusById - Internal error", slog.String("error", err.Error()))
-		return nil, ErrInternal
-	}
-
-	//How many users will we assign as reviewers
-	var reviewersId []string
-	var count int
-	if len(activeUsers) > 2 {
-		count = 2
-	} else {
-		count = len(activeUsers)
-	}
-
-	//Randomly shuffle the slice of active users and assign reviewers
-	perm := rand.Perm(len(activeUsers))
-	for i := 0; i < count; i++ {
-		reviewersId = append(reviewersId, activeUsers[perm[i]].UserId)
-	}
-
-	err = s.prRepo.AddReviewers(ctx, pr.PrId, reviewersId)
-	if err != nil {
-		if errors.Is(err, repository.ErrAlreadyExists) {
-			return nil, ErrPullRequestALreadyExists
-		}
-		s.logger.Error("PullRequestService.Create:prRepo.AddReviewers - Internal error", slog.String("error", err.Error()))
-		return nil, ErrInternal
-	}
-
-	return &dto.PullRequest{
-		PrId:              p.PrId,
-		PrName:            p.Name,
-		AuthorId:          p.AuthorId,
-		Status:            status,
-		AssignedReviewers: reviewersId,
-	}, nil
+	return resp, err
 }
 
 func (s *PullRequestService) Merge(ctx context.Context, prId string) (*dto.MergeResponse, error) {
