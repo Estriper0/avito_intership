e9ef1d8cb5550ac4f63da508b8dd1a7096c65498
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-23 06:18:49 +0300

    Removed loading from ENV from the file, everything is in docker-compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Estriper0/avito_intership/internal/app"
 	"github.com/Estriper0/avito_intership/internal/config"
 	"github.com/Estriper0/avito_intership/internal/logger"
-	"github.com/joho/godotenv"
 )
 
 const configPath = "configs/config.yaml"
 
 func main() {
-	_ = godotenv.Load()
-
 	config := config.New(configPath)
+	fmt.Println(config)
 	logger := logger.GetLogger(config.App.Env)
-	
+
 	app := app.New(logger, config)
 	app.Run()
 }
